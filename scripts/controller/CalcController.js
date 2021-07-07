595342bfa93b1afde3c62979ffc6ed31c627cfde
eduardoreis <eduardo.reoli1983@gmail.com>
2021-07-07 15:07:51 -0300

    Calculadora getElementsById

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,31 @@
+class CalcController {
+    constructor(){
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+        let displayCalcEl = document.querySelector("#display");
+        let timeCalcEl = document.querySelector("#hora");
+        let dateCalcEl = document.querySelector("#data");
+
+        displayCalcEl.innerHTML = "Eduardo"
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(valor){
+        this._currentDate = valor;
+    }
+}
